3b7d552d9faa3333bb1a558e2d35a938f51baead
Chris Brown <cbrown@weyerhaeuser.com>
2018-02-08 17:08:03 -0700

    Get Rush Order Prices from text file and load the RushOrderPrices array.

--- MegaDesk2-MelissaMoakeChrisBrown/DeskQuote.cs
+++ MegaDesk2-MelissaMoakeChrisBrown/DeskQuote.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MegaDesk2_MelissaMoakeChrisBrown
 {
@@ -13,11 +15,11 @@ namespace MegaDesk2_MelissaMoakeChrisBrown
         public int RushDays { get; set; }
         public Desk Desk = new Desk();
         public decimal QuotePrice { get; set; }
-        private int[,] RushDayPrices = new int[,] { { 60, 70, 80 }, { 40, 50, 60 }, { 30, 35, 40 } };
-
+        //private int[,] RushDayPrices = new int[,] { { 60, 70, 80 }, { 40, 50, 60 }, { 30, 35, 40 } };
+        private int[,] RushDayPrices;
         private decimal SurfaceArea = 0;
 
-        //Constrants go here
+        //Constants go here
         private const int BASE_PRICE = 200;
         private const int BASE_SIZE_MAX = 1000;
         private const int PRICE_PER_DRAWER = 50;
@@ -28,7 +30,7 @@ namespace MegaDesk2_MelissaMoakeChrisBrown
         private const int RUSH_LARGE_SIZE = 2000; // Use to help with rush prices
 
 
-        public DeskQuote(string name, DateTime quoteDate, decimal width, decimal depth, 
+        public DeskQuote(string name, DateTime quoteDate, decimal width, decimal depth,
             int drawers, MaterialType material, int rushDays)
         {
             CustomerName = name;
@@ -53,7 +55,7 @@ namespace MegaDesk2_MelissaMoakeChrisBrown
             if (SurfaceArea > BASE_SIZE_MAX)
             {
                 extraSurfaceCost = (SurfaceArea - BASE_SIZE_MAX) * PRICE_PER_SQUAREAREA;
-               
+
             }
             return extraSurfaceCost;
         }
@@ -67,54 +69,93 @@ namespace MegaDesk2_MelissaMoakeChrisBrown
         {
             int rushPrice = 0;
 
-            switch (RushDays)
+            if (RushDays != 0)
             {
-                case RUSH_OPTION_1:
-                    if (SurfaceArea < BASE_SIZE_MAX)
-                    {
-                        rushPrice = RushDayPrices[0, 0];
-                    }
-                    else if (SurfaceArea <= RUSH_LARGE_SIZE){
-                        rushPrice = RushDayPrices[0, 1];
-                    }
-                    else
-                    {
-                        rushPrice = RushDayPrices[0, 2];
-                    }
-                break;
 
-                case RUSH_OPTION_2:
-                    if (SurfaceArea < BASE_SIZE_MAX)
-                    {
-                        rushPrice = RushDayPrices[1, 0];
-                    }
-                    else if (SurfaceArea <= RUSH_LARGE_SIZE)
-                    {
-                        rushPrice = RushDayPrices[1, 1];
-                    }
-                    else
-                    {
-                        rushPrice = RushDayPrices[1, 2];
-                    }
-                break;
+                RushDayPrices = GetRushOrderPrices();  //load current rush order prices
 
-                case RUSH_OPTION_3:
-                    if (SurfaceArea < BASE_SIZE_MAX)
-                    {
-                        rushPrice = RushDayPrices[2, 0];
-                    }
-                    else if (SurfaceArea <= RUSH_LARGE_SIZE)
-                    {
-                        rushPrice = RushDayPrices[2, 1];
-                    }
-                    else
-                    {
-                        rushPrice = RushDayPrices[2, 2];
-                    }
-                break;
+                switch (RushDays)
+                {
+                    case RUSH_OPTION_1:
+                        if (SurfaceArea < BASE_SIZE_MAX)
+                        {
+                            rushPrice = RushDayPrices[0, 0];
+                        }
+                        else if (SurfaceArea <= RUSH_LARGE_SIZE)
+                        {
+                            rushPrice = RushDayPrices[0, 1];
+                        }
+                        else
+                        {
+                            rushPrice = RushDayPrices[0, 2];
+                        }
+                        break;
+
+                    case RUSH_OPTION_2:
+                        if (SurfaceArea < BASE_SIZE_MAX)
+                        {
+                            rushPrice = RushDayPrices[1, 0];
+                        }
+                        else if (SurfaceArea <= RUSH_LARGE_SIZE)
+                        {
+                            rushPrice = RushDayPrices[1, 1];
+                        }
+                        else
+                        {
+                            rushPrice = RushDayPrices[1, 2];
+                        }
+                        break;
+
+                    case RUSH_OPTION_3:
+                        if (SurfaceArea < BASE_SIZE_MAX)
+                        {
+                            rushPrice = RushDayPrices[2, 0];
+                        }
+                        else if (SurfaceArea <= RUSH_LARGE_SIZE)
+                        {
+                            rushPrice = RushDayPrices[2, 1];
+                        }
+                        else
+                        {
+                            rushPrice = RushDayPrices[2, 2];
+                        }
+                        break;
+                }
             }
             return rushPrice;
         }
+        private int[,] GetRushOrderPrices()
+        {
+            int[,] rushPrices = new int[3, 3];
+            int i = 0, j = 0;
+
+            try
+            {
+                string[] prices = File.ReadAllLines("rushOrderPrices.txt");
+
+                foreach (var price in prices)
+                {
+                    int value;
+                    if (Int32.TryParse(price, out value))
+                    {
+                        rushPrices[i, j] = value;
+                        j++;
+                        if (j > 2)
+                        {
+                            i++;
+                            j = 0;
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "Error loading Rush Order Pricing"); // Tell user about the error
+            }
+            return rushPrices;
+        }
+
     }
-    
 }
